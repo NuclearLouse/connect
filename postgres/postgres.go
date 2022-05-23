d270892c26749df19c9c04764dbaf039f1656a85
asorokin <a40in@inbox.ru>
2022-05-23 18:22:26 +0300

    Initial commit

---
+++ postgres/postgres.go
@@ -0,0 +1,63 @@
+package postgres
+
+import (
+	"context"
+	"fmt"
+	"net/url"
+
+	"github.com/jackc/pgx/v4/pgxpool"
+)
+
+type Config struct {
+	User         string
+	Pass         string
+	Host         string
+	Port         int
+	Database     string
+	SSLMode      string
+	PoolMaxConns int
+}
+
+//DefaultConfig return
+// Host:         "localhost",
+// Port:         5432,
+// Database:     "postgres",
+// User:         "postgres",
+// Pass:         "postgres",
+// SSLMode:      "disable",
+// PoolMaxConns: 10,
+func DefaultConfig() *Config {
+	return &Config{
+		Host:         "localhost",
+		Port:         5432,
+		Database:     "postgres",
+		User:         "postgres",
+		Pass:         "postgres",
+		SSLMode:      "disable",
+		PoolMaxConns: 10,
+	}
+}
+
+func Connect(ctx context.Context, cfg *Config) (*pgxpool.Pool, error) {
+
+	val := url.Values{}
+	val.Set("sslmode", cfg.SSLMode)
+	val.Set("pool_max_conns", fmt.Sprintf("%d", cfg.PoolMaxConns))
+	url := url.URL{
+		Scheme:   "postgres",
+		Host:     fmt.Sprintf("%s:%d", cfg.Host, cfg.Port),
+		User:     url.UserPassword(cfg.User, cfg.Pass),
+		Path:     cfg.Database,
+		RawQuery: val.Encode(),
+	}
+
+	pool, err := pgxpool.Connect(ctx, url.String())
+	if err != nil {
+		return nil, err
+	}
+
+	// pool.Config().MaxConnIdleTime = 60 * time.Second
+	// pool.Config().MaxConnLifetime = 5 * time.Minute
+
+	return pool, pool.Ping(ctx)
+}
